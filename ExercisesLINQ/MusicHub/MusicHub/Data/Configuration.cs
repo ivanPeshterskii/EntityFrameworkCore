aee8dbf6eacf31ee7c087748f48231aa3c55ed54
Ivan Pehsterski <peshterskiivan@gmail.com>
2026-08-01 11:58:18 +0300

    Delete ExercisesLINQ directory

--- ExercisesLINQ/MusicHub/MusicHub/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicHub.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=localhost;Database=MusicHub;Trusted_Connection=True";
-    }
-}
